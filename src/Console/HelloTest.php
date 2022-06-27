276c41b7d3d0d9b3d49c5e8285ea13394e1a3316
wan8899 <229643466@qq.com>
2022-06-27 23:07:00 +0800

    gg

--- src/Console/HelloTest.php
+++ src/Console/HelloTest.php
@@ -4,6 +4,7 @@ namespace Wanecho\Hello\Console;
 
 use Flarum\Console\AbstractCommand;
 use Flarum\Post\Post;
+use itbdw\Ip\IpLocation;
 
 class HelloTest extends AbstractCommand 
 {
@@ -15,7 +16,7 @@ class HelloTest extends AbstractCommand
     }
 
     protected function fire()
-    {
+    {   
         // $ip = '114.101.232.16';
         // $res =  Post::where('ip_address',$ip)->groupBy('ip_address')->first();
         // if(!empty($res->ip_city)){
@@ -24,7 +25,7 @@ class HelloTest extends AbstractCommand
         //http://ip-api.com/json/24.28.24.176?lang=zh-CN
         $rows =  Post::whereNull('ip_city')->groupBy('ip_address')->orderBy('id', 'desc')->get();
         foreach( $rows as $row ){
-           $city =  $this->getcity($row->ip_address);
+           echo $city =  $this->getcity($row->ip_address);
            if(!empty($city)){
                 Post::where('ip_address',$row->ip_address)->update(['ip_city'=>$city]);
            }
@@ -32,19 +33,10 @@ class HelloTest extends AbstractCommand
     }
 
     private function getcity($ip){
-
-        $url = 'http://ip-api.com/json/'.$ip.'?lang=zh-CN';
-        try {
-            echo $result =  file_get_contents($url);
-            $result =  json_decode($result,true);
-            if($result['status']=='success'){
-                //国内显示城市  国外显示 地区
-                return $result['countryCode']=='CN'?$result['city']:$result['regionName'];
-            }
-            return "";
-        } catch (\Throwable $th) {
-            echo $th->getMessage();
-            return false;
+        $ipdata =  IpLocation::getLocation($ip);
+        if(!isset($ipdata['error'])){
+            return $ipdata['city'];
         }
+        return "";
     }
 }
